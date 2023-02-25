2854f62c5daf820e6c6918fe758845e3bb0cd8ed
sz <tatsuya.suzuki.jp@gmail.com>
2023-02-25 13:03:07 +0900

    add address
    test

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TCHPDXHGRAAL6HFRHDVFXKTFXOA7TI7IP4NEFZA")
+
 console.log("Hallo Symbol")
+
+console.log('Your Address: ${address.plain()}')
+
