24c1d6028ce6665a1d3de9850c897668be272573
sz <tatsuya.suzuki.jp@gmail.com>
2023-02-27 23:45:43 +0900

    change
    test

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const symbol = require('/node_modules/symbol-sdk');
+const sym = require('/node_modules/symbol-sdk');
 
 
 const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836';
 const EPOCH = 1637848847;
 const XYM_ID = '3A8416DB2D53B6C8';
 const NODE_URL = 'https://sym-test.opening-line.jp:3001';
-const NET_TYPE = symbol.NetworkType.TEST_NET;
+const NET_TYPE = sym.NetworkType.TEST_NET;
 
-const repo = new symbol.RepositoryFactoryHttp(NODE_URL);
+const repo = new sym.RepositoryFactoryHttp(NODE_URL);
 const accountRepo = repo.createAccountRepository();
 const tsRepo = repo.createTransactionRepository();
 
@@ -25,8 +25,14 @@ function LoadSSS(){
     console.log('SSS is Allowed');
 
     const address = window.SSS.activeAddress;
+
+    myAddress = sym.Address.createFromRawAddress(
+      window.SSS.activeAddress
+    );
+    console.log(myAddress);  
+
     (async() =>{
-      const accountInfo = await accountRepo.getAccountInfo(address).toPromise() ;
+      const accountInfo = await accountRepo.getAccountInfo(myAddress).toPromise() ;
       console.log(accounInfo);
     })();
 
