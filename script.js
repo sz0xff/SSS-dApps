8bab5ead949bff7754cf096d3941277077a91d81
sz <tatsuya.suzuki.jp@gmail.com>
2023-02-27 23:31:22 +0900

    change
    test

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const symbol = require('/node_modules/symbol-sdk')
+const symbol = require('/node_modules/symbol-sdk');
 
 
-const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836'
-const EPOCH = 1637848847
-const XYM_ID = '3A8416DB2D53B6C8'
-const NODE_URL = 'https://sym-test.opening-line.jp:3001'
-const NET_TYPE = symbol.NetworkType.TEST_NET
+const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836';
+const EPOCH = 1637848847;
+const XYM_ID = '3A8416DB2D53B6C8';
+const NODE_URL = 'https://sym-test.opening-line.jp:3001';
+const NET_TYPE = symbol.NetworkType.TEST_NET;
 
-const repo = new symbol.RepositoryFactoryHttp(NODE_URL)
-const accountRepo = repo.createAccountRepository()
-const tsRepo = repo.createTransactionRepository()
+const repo = new symbol.RepositoryFactoryHttp(NODE_URL);
+const accountRepo = repo.createAccountRepository();
+const tsRepo = repo.createTransactionRepository();
 
 
 function LoadSSS(){
@@ -25,7 +25,7 @@ function LoadSSS(){
     console.log('SSS is Allowed');
 
     const address = window.SSS.activeAddress;
-    const accountInfo = await accountRepo.getAccountInfo(address).toPromise();
+    const accountInfo = accountRepo.getAccountInfo(address);
     console.log(accounInfo);
 
   }else{
