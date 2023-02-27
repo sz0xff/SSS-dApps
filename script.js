ac69cdbc9d26614d7d2cc98dfbb3397bfbb3b415
sz <tatsuya.suzuki.jp@gmail.com>
2023-02-27 20:39:41 +0900

    script change
    test

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+/*
 const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836'
 const EPOCH = 1637848847
 const XYM_ID = '3A8416DB2D53B6C8'
@@ -9,16 +10,10 @@ const NET_TYPE = symbol.NetworkType.TEST_NET
 const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
 const accountHttp = repositoryFactory.createAccountRepository()
 const transactionHttp = repositoryFactory.createTransactionRepository()
-
+*/
 
 function LoadSSS(){
   console.log('LoadSSS');
-
-  const address = symbol.Address.createFromRawAddress(window.SSS.activeAddress);
-  const addressPretty = address.pretty();
-
-  console.log(address);
-  console.log(addressPretty);
 
   console.log(window.SSS);
 }
