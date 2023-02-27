48d41bef67be6475f4a18223e6310029300ada57
sz <tatsuya.suzuki.jp@gmail.com>
2023-02-27 20:19:56 +0900

    change something
    test

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 const symbol = require('/node_modules/symbol-sdk')
-const address = symbol.Address.createFromRawAddress("TCHPDXHGRAAL6HFRHDVFXKTFXOA7TI7IP4NEFZA")
 
-console.log("Hallo Symbol")
-
-console.log(`Your Address: ${address.plain()}`)
-
-const dom_addr = document.getElementById('wallet-addr')
-dom_addr.innerText = address.pretty()
-
-
-console.log(address.pretty())
-
-console.log("test")
-
-//testnet information
 const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836'
 const EPOCH = 1637848847
 const XYM_ID = '3A8416DB2D53B6C8'
 const NODE_URL = 'https://sym-test.opening-line.jp:3001'
 const NET_TYPE = symbol.NetworkType.TEST_NET
 
-
 const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
 const accountHttp = repositoryFactory.createAccountRepository()
+const transactionHttp = repositoryFactory.createTransactionRepository()
 
-accountHttp.getAccountInfo(address)
-  .toPromise()
-  .then((accountInfo) => {
-    for (let m of accountInfo.mosaics) {
-      if (m.id.id.toHex() === XYM_ID) {
-        const dom_xym = document.getElementById('wallet-xym')
-        dom_xym.innerText = `XYM Balance : ${m.amount.compact() / Math.pow(10, 6)}`
-      }
-    }
-  })
+setTimeout(() => {
+  
+const address = symbol.Address.createFromRawAddress(window.SSS.activeAddress)
+const addressPretty = address.pretty();
+
+console.log(address);
+console.log(addressPretty);
+
+},500);
 
