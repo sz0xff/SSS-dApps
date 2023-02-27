bb73c47a493a15ed0df5dc97bc3020cbf60661ed
sz <tatsuya.suzuki.jp@gmail.com>
2023-02-28 00:00:53 +0900

    some change
    test

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const sym = require('/node_modules/symbol-sdk');
 const GENERATION_HASH = '7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836';
 const EPOCH = 1637848847;
 const XYM_ID = '3A8416DB2D53B6C8';
-const NODE_URL = 'https://sym-test.opening-line.jp:3001';
+const NODE_URL = 'https://sym-test-04.opening-line.jp:3001';
 const NET_TYPE = sym.NetworkType.TEST_NET;
 
 const repo = new sym.RepositoryFactoryHttp(NODE_URL);
